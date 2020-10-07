8baafb64810e4eef23b1492e204a56665b93f01b
Developer <you@example.com>
2020-10-07 03:34:56 +0330

    Task Section4 Lambda

--- app/src/main/java/com/sematec/taskimdb/MainActivity.kt
+++ app/src/main/java/com/sematec/taskimdb/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.sematec.taskimdb
 
-import android.icu.text.CaseMap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
-import java.time.Year
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val film1 = Movie(
             "Chiz-haie hast keh nemidani",
             "2010",
@@ -42,15 +40,30 @@ class MainActivity : AppCompatActivity() {
             R.drawable.mv4
         )
 
-        var flist = ArrayList<Movie>()
-        flist.add(film1)
-        flist.add(film2)
-        flist.add(film3)
-        flist.add(film4)
-        var adapter = imdbAdapter(flist)
-        recycler.adapter = adapter
+        var filmlist = ArrayList<Movie>()
+        filmlist.add(film1)
+        filmlist.add(film2)
+        filmlist.add(film3)
+        filmlist.add(film4)
+        //var adapter = imdbAdapter(filmist)
+        // recycler.adapter = imdbAdapter(filmist, {movie : Movie -> movieclicked(movie) })
+        recycler.adapter = imdbAdapter(filmlist, movieclicked)
         recycler.layoutManager =
             LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
 
+        /* val intent = getIntent()
+         val movietitle = intent.getStringExtra("movietitle")
+         Toast.makeText(this@MainActivity, "Movie :  ${movietitle}", Toast.LENGTH_SHORT).show() */
+
     }
+
+    val movieclicked: (Movie) -> Unit = { movie ->
+        Toast.makeText(this@MainActivity, "Movie Title is: ${movie.Title}", Toast.LENGTH_SHORT)
+            .show()
+    }
+
+    /*private fun movieClicked(movie : Movie) {
+    Toast.makeText(this, "Movie Title is: ${movie.Title}", Toast.LENGTH_SHORT).show()
+}*/
+
 }
--- app/src/main/java/com/sematec/taskimdb/imdbAdapter.kt
+++ app/src/main/java/com/sematec/taskimdb/imdbAdapter.kt
@@ -6,12 +6,10 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.recycler_item.view.*
 
 
-class imdbAdapter(private var list: ArrayList<Movie>) :
+class imdbAdapter(var list: ArrayList<Movie>, val clickListener: (Movie) -> Unit) :
     RecyclerView.Adapter<imdbAdapter.imdbViewHolder>() {
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): imdbViewHolder {
 
         val inflater = LayoutInflater.from(parent.context)
@@ -20,7 +18,7 @@ class imdbAdapter(private var list: ArrayList<Movie>) :
     }
 
     override fun onBindViewHolder(holder: imdbViewHolder, position: Int) =
-        holder.bind(list[position])
+        holder.bind(list[position], clickListener)
 
     override fun getItemCount() = list.size
 
@@ -31,17 +29,28 @@ class imdbAdapter(private var list: ArrayList<Movie>) :
         private val txtdirector: TextView = itemView.findViewById(R.id.txtDirector)
         private val txtactors: TextView = itemView.findViewById(R.id.txtActors)
         private val imgposter: ImageView = itemView.findViewById(R.id.imgPoster)
-        fun bind(movie: Movie) {
+        fun bind(movie: Movie, clickListener: (Movie) -> Unit) {
+
             txttile.text = movie.Title
             txtyear.text = movie.Year
             txtdirector.text = movie.Director
             txtactors.text = movie.Actors
             imgposter.setImageResource(movie.Poster)
+
+            itemView.setOnClickListener {
+                clickListener(movie)
+
+                //val position: Int = adapterPosition
+                /* Toast.makeText(itemView.context, "Movie Title is :  ${movie.Title}", Toast.LENGTH_SHORT).show()
+                val intent = Intent()
+                intent.setClass(itemView.context,MainActivity::class.java)
+                intent.putExtra("movietitle", movie.Title)*/
+            }
         }
 
     }
-
 }
+
 
 
 
